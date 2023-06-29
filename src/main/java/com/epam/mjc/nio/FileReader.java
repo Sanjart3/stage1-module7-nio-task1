5ab38f69019344f3f90e685402b56a205f0a6dab
stcoderts <115059135+stcoderts@users.noreply.github.com>
2023-06-29 17:38:46 +0500

    first commit

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,33 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
+
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile profile = new Profile();
+        try {
+            BufferedReader bf = new BufferedReader(new java.io.FileReader(file));
+            String line = bf.readLine();
+            while (line != null){
+                String value = line.split(":")[1];
+                switch(line.split(":")[0]){
+                    case "Name": profile.setName(value.trim()); break;
+                    case "Age": profile.setAge(Integer.valueOf(value.trim())); break;
+                    case "Email": profile.setEmail(value.trim()); break;
+                    case "Phone": profile.setPhone(Long.valueOf(value.trim())); break;
+                    default: line=null;
+                }
+                line = bf.readLine();
+            }
+        } catch (IOException e) {
+            Logger.getLogger(String.valueOf(e));
+        }
+        return profile;
     }
 }
